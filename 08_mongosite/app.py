876492624210bb53983568d040ac7b5d88a3cd77
Max Millar <mmillar@stuy.edu>
2019-03-07 17:33:24 +0000

    k08 work

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 
+import nobel
+
 app = Flask(__name__)
 
 @app.route('/')
 def render_test():
     return "Hello World!"
 
+@app.route('/data')
+def dataRet():
+    ret =nobel.getYear()
+    return ret
+    
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
