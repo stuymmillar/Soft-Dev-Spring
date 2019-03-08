4d1186eaf4105e3c8f07f7efe963babe50b1255f
Max Millar <mmillar@stuy.edu>
2019-03-08 01:10:35 +0000

    did k08

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -1,16 +1,25 @@
-from flask import Flask
+#Team Rewind Time - Max Millar, Kendrick Liang, Jiajie Mai
+#SoftDev2 pd06
+#k08 -- Ay Mon, Go Git It From Yer Flask
+#2019-03-08
 
-import nobel
+from flask import Flask, render_template, request
+
+from util import nobel as n
 
 app = Flask(__name__)
 
+n.dropDatabase()
+
 @app.route('/')
 def render_test():
-    return "Hello World!"
+    return render_template('index.html')
 
-@app.route('/data')
+@app.route('/data', methods=['POST','GET'])
 def dataRet():
-    ret =nobel.getYear()
+    query = request.args['query']
+    ip = request.args['ip']
+    ret = n.queryYear(query, ip)
     return ret
     
 if __name__ == '__main__':
