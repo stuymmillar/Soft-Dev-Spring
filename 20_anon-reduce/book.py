20acd1ef66eebeacb1f6ddb8facf75579d4539dd
Max Millar <mmillar@stuy.edu>
2019-04-18 12:35:12 -0400

    some k20 work

--- 20_anon-reduce/book.py
+++ 20_anon-reduce/book.py
@@ -2,10 +2,9 @@ from functools import reduce
 import os
 import re
 
-enc = 'utf-8'
-with open('moby.txt','r', encoding=enc) as f:
+with open('moby.txt','r') as f:
     word = f.read()
-words = [x for x in re.split('[ \r\n]', word) if x != '']
+words = [x.lower() for x in re.split('[ \r\n,.?!;]', word) if x != '']
 
 
 #print(words)
@@ -13,24 +12,20 @@ words = [x for x in re.split('[ \r\n]', word) if x != '']
 def wordFreq(w):
     return reduce((lambda x,y : x + y), [1 for x in words if x == w])
 
-print(wordFreq('Ahab'))
+#print(wordFreq('Ahab'))
 
 def wordsFreq(w):
     ws = [x for x in re.split('[ \r\n]', w) if x != '']
     return reduce((lambda x,y : x + y), [1 for x in [x for x in range(len(words)) if words[x] == ws[0]] if words[x:x + len(ws)] == ws])
 
-print(wordsFreq("Captain Ahab"))
+#print(wordsFreq("Captain Ahab"))
 
 
+#def mostFreq():
+    s = list(set(words))
+    freqs = [0 for a in range(len(s))]
+    [freqs[s.index(x)] := freqs[s.index(x)] + 1 for x in words ]
+    copy = freqs.sort(-1)
+    return s[freqs.index(copy[0])]
 
-def mostFreq():
-    w = ''
-    n = 0
-    for x in list(set(words)):
-        n2 = wordFreq(x)
-        if n2 > n:
-            n = n2
-            w = x
-    return w
-
-print(mostFreq())
+#print(mostFreq())
